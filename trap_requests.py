2a6e8092bc822b4c7b5ed0d14f500713e03fe810
maxanatsko <maxanatsko@gmail.com>
2023-11-21 18:42:26 +0100

    Added fetching and writing for templates, milestones, suites and plans

--- trap_requests.py
+++ trap_requests.py
@@ -21,15 +21,35 @@ def main():
             file_writer.write_data_to_file("projects.json", projects)
 
             # Additional data collection and file writing
-            # ...
+            case_types = methods.get_case_types()
+            file_writer.write_data_to_file("case_types.json", case_types)
 
+            statuses = methods.get_statuses()
+            file_writer.write_data_to_file("statuses.json", statuses)
+
+            priorities = methods.get_priorities()
+            file_writer.write_data_to_file("priorities.json", priorities)
+
+            # Process filtered projects
             filtered_projects = [d for d in projects if d['name'] == project_name]
             if filtered_projects:
                 project_id = filtered_projects[0]["id"]
+
+                # Fetching and writing templates data
                 templates = methods.get_templates(project_id)
                 file_writer.write_data_to_file("templates.json", templates)
-                # Additional processing for milestones, suites, etc.
-                # ...
+
+                # Fetching and writing milestones data
+                milestones = methods.get_milestones(project_id)
+                file_writer.write_data_to_file("milestones.json", milestones)
+
+                # Fetching and writing suites data
+                suites = methods.get_suites(project_id)
+                file_writer.write_data_to_file("suites.json", suites)
+
+                # Fetching and writing plans data
+                plans = methods.get_plans(project_id)
+                file_writer.write_data_to_file("plans.json", plans)
 
             end_time = datetime.datetime.now()
             duration = end_time - start_time
